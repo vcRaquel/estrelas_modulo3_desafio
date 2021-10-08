ff643a123fba853d8f76d9fbc9d69b025d536178
Raquel Leão <vc.raquel@gmail.com>
2021-10-08 11:34:02 -0300

    definir esqueleto classe servicoCliente

---
+++ src/br/com/zup/ServicoCliente.java
@@ -0,0 +1,7 @@
+package br.com.zup;
+
+public class ServicoCliente {
+    //listar todos clientes - lista de clientes - fazer exibição percorrendo a lista e printando a referencia
+    //cadastrar cliente - método composto com a classe sistema para cadastrar um cliente - a lista garantirá a multiplicidade de cadastros
+    //verificar se o cliente está cadastrado - percorrer a lista de vendedores comparando cpf ou email;
+}
