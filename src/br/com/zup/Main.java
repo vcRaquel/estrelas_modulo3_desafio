1590ff6d0b0b786645f9368b1332689b125f427e
Raquel Leão <vc.raquel@gmail.com>
2021-10-08 19:01:43 -0300

    rodar o método executar com um menu com todas as opções

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,21 +3,16 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-//           System.out.println(ValidaDado.tratarCpf("36854+ - 8567"));
-            System.out.println(Sistema.cadastrarVendedor());
-            System.out.println(Sistema.cadastrarCliente());
-            System.out.println(Sistema.cadastrarVenda());
-            System.out.println(Sistema.cadastrarVenda());
-            System.out.println(ServicoVenda.comprasCliente("1"));
-            System.out.println(ServicoVenda.vendasVendedor("@"));
+        boolean continuar = true;
 
-//           System.out.println(Sistema.cadastrarVenda());
-//           System.out.println("==========================");
-//           ServicoVenda.exibirVendas();
-        } catch (Exception erro) {
-            System.out.println(erro.getMessage());
+        while (continuar) {
+            try {
+                continuar = Sistema.executar();
+            } catch (Exception erro) {
+                System.out.println(erro.getMessage());
+            }
         }
+
 
     }
 }
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Sistema {
@@ -12,17 +14,19 @@ public class Sistema {
     //menu inicial
     public static void menu() {
         System.out.println("Programa de gerenciamento de vendas: \n");
-        System.out.println("Digite 1 para cadastrar um funcionário");
+        System.out.println("Digite 1 para cadastrar um vendedor");
         System.out.println("Digite 2 para cadastrar um cliente");
         System.out.println("Digite 3 para cadastrar uma venda");
-        System.out.println("digite 4 para exibir a lista de funcionários");
+        System.out.println("digite 4 para exibir a lista de vendedores");
         System.out.println("digite 5 para exibir a lista de clientes");
-        System.out.println("digite 4 para exibir a lista de faturas");
-        System.out.println("digite 7 para sair");
+        System.out.println("digite 6 para exibir a lista de vendas");
+        System.out.println("digite 7 para exibir a lista de vendas de um vendedor");
+        System.out.println("digite 8 para exibir a lista de compras de um cliente");
+        System.out.println("digite 9 para sair");
     }
 
     //cadastrar vendedor - método composto com a classe ServicoVendedor
-    public static Vendedor cadastrarVendedor() throws Exception{
+    public static Vendedor cadastrarVendedor() throws Exception {
         String nome = capturarDados("Digite o nome do vendedor: ").nextLine();
         String email = capturarDados("Digite o email do vendedor: ").nextLine();
         String cpf = capturarDados("Digite o CPF do vendedor: ").nextLine();
@@ -30,7 +34,7 @@ public class Sistema {
     }
 
     //cadastrar cliente - método composto com a classe ServicoCliente
-    public static Cliente cadastrarCliente() throws Exception{
+    public static Cliente cadastrarCliente() throws Exception {
         String nome = capturarDados("Digite o nome do cliente: ").nextLine();
         String email = capturarDados("Digite o email do cliente: ").nextLine();
         String cpf = capturarDados("Digite o CPF do cliente: ").nextLine();
@@ -38,12 +42,51 @@ public class Sistema {
     }
 
     //cadastrar venda - método composto com a classe ServicoVenda
-    public static Venda cadastrarVenda() throws Exception{
+    public static Venda cadastrarVenda() throws Exception {
         System.out.println("\n Registro de venda: ");
         String cpfCliente = capturarDados("Digite o CPF do cliente: ").nextLine();
         String emailVendedor = capturarDados("Digite o email do vendedor: ").nextLine();
         double valorAPagar = capturarDados("Digite o valor a ser pago: ").nextDouble();
         String dataRegistro = capturarDados("Digite a data de registro da venda: ").nextLine();
         return ServicoVenda.cadastrarVenda(cpfCliente, emailVendedor, valorAPagar, dataRegistro);
+    }
+
+    public static boolean executar() throws Exception {
+        boolean seletor = true;
+        List<Vendedor> vendedores = new ArrayList<>();
+        List<Cliente> clientes = new ArrayList<>();
+        List<Venda> vendas = new ArrayList<>();
+        while (seletor) {
+            menu();
+            int opcaoUsuario = capturarDados("Digite a opção desejada").nextInt();
+            if (opcaoUsuario == 1) {
+                Vendedor vendedor = cadastrarVendedor();
+                System.out.println(vendedor);
+            } else if (opcaoUsuario == 2) {
+                Cliente cliente = cadastrarCliente();
+            } else if (opcaoUsuario == 3) {
+                Venda venda = cadastrarVenda();
+            } else if (opcaoUsuario == 4) {
+                ServicoVendedor.exibirVendedores();
+            } else if (opcaoUsuario == 5) {
+                ServicoCliente.exibirClientes();
+            } else if (opcaoUsuario == 6) {
+                ServicoVenda.exibirVendas();
+            } else if (opcaoUsuario == 7) {
+                //mostrar vendas de um vendedor
+                String email = capturarDados("Digite o email do vendedor: ").nextLine();
+                List<Venda> vendasVendedor = ServicoVenda.vendasVendedor(email);
+                System.out.println(vendasVendedor);
+            } else if (opcaoUsuario == 8) {
+                //mostrar compras de um cliente
+                String cpf = capturarDados("Digite o CPF do cliente").nextLine();
+                List<Venda> comprasCliente = ServicoVenda.comprasCliente(cpf);
+                System.out.println(comprasCliente);
+            } else if (opcaoUsuario == 9) {
+                System.out.println("Finalizando programa de gestão de vendas");
+                seletor = false;
+            }
+        }
+        return seletor;
     }
 }
