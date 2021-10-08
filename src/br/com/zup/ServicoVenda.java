c71fa42c58804f894f131fec4221325f4f94a2b3
Raquel Leão <vc.raquel@gmail.com>
2021-10-08 11:27:38 -0300

    definir esqueleto classe servicoVenda

---
+++ src/br/com/zup/ServicoVenda.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class ServicoVenda {
+    //listar todas as vendas cadastradas - providenciar lista de vendas - serve também para cadastrar mais de uma venda no sistema
+    //não cadastrar vendas para clientes não cadastrados - providenciar lista de clientes - percorrer a lista comparando cpf ou email
+    //não cadastrar vendas de vendedores não cadastrados - providenciar lista de vendedores - percorrer a lista comparando cpf ou email
+    //cadastrar diversas vendas no sistema - método composto com a classe sistema para cadastrar uma venda, a lista se encarregará de garantir a multiplicidade de cadastro
+
+}
