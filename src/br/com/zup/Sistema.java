2d743c133f4e5662a2c33881f653e0605d5d4909
Raquel Leão <vc.raquel@gmail.com>
2021-10-08 11:48:31 -0300

    exibir opções do menu

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -9,6 +9,16 @@ public class Sistema {
         return new Scanner(System.in);
     }
     //menu inicial
+    public static void menu(){
+        System.out.println("Programa de gerenciamento de vendas: \n");
+        System.out.println("Digite 1 para cadastrar um funcionário");
+        System.out.println("Digite 2 para cadastrar um cliente");
+        System.out.println("Digite 3 para cadastrar uma venda");
+        System.out.println("digite 4 para exibir a lista de funcionários");
+        System.out.println("digite 5 para exibir a lista de clientes");
+        System.out.println("digite 4 para exibir a lista de faturas");
+        System.out.println("digite 7 para sair");
+    }
     //cadastrar vendedor
     //cadastrar cliente
     //cadastrar venda
