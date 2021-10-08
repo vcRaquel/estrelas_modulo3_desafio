865763d9d905575989f213488783a0ac0e9fa341
Raquel Leão <vc.raquel@gmail.com>
2021-10-08 11:08:56 -0300

    definir classe vendedor

---
+++ src/br/com/zup/Vendedor.java
@@ -0,0 +1,19 @@
+package br.com.zup;
+
+public class Vendedor extends Pessoa{
+    public Vendedor() {
+    }
+
+    public Vendedor(String nome, String email, String cpf) {
+        super(nome, email, cpf);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n Nome do vendedor: " + super.getNome());
+        retorno.append("\n Email do vendedor: " + super.getEmail());
+        retorno.append("\n CPF do vendedor: " + super.getCpf());
+        return retorno.toString();
+    }
+}
