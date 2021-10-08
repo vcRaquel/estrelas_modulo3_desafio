b293e2442757576d74cb5070e424939dd6ffc4bd
Raquel Leão <vc.raquel@gmail.com>
2021-10-08 11:32:30 -0300

    definir esqueleto classe servicoVendedor

---
+++ src/br/com/zup/ServicoVendedor.java
@@ -0,0 +1,7 @@
+package br.com.zup;
+
+public class ServicoVendedor {
+    //listar todos vendedores - lista de vendedores - fazer exibição percorrendo a lista e printando a referencia
+    //cadastrar vendedores - método composto com a classe sistema para cadastrar um vendedor - a lista garantirá a multiplicidade de cadastros
+    //verificar se o vendedor está cadastrado - percorrer a lista de vendedores comparando cpf ou email;
+}
