ca0353f545e2b1323e8643d9fdb8ea41b9adcf65
Raquel Leão <vc.raquel@gmail.com>
2021-10-08 12:28:35 -0300

    listar vendedores cadastrados

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(Sistema.cadastrarVendedor());
+        System.out.println(Sistema.cadastrarVendedor());
+        ServicoVendedor.exibirVendedores();
     }
 }
--- src/br/com/zup/ServicoVendedor.java
+++ src/br/com/zup/ServicoVendedor.java
@@ -5,13 +5,22 @@ import java.util.List;
 
 public class ServicoVendedor {
     private static List<Vendedor> vendedores = new ArrayList<>();
-    //listar todos vendedores - lista de vendedores - fazer exibição percorrendo a lista e printando a referencia
+
 
     //cadastrar vendedor - método composto com a classe sistema para cadastrar um vendedor - a lista garantirá a multiplicidade de cadastros
-    public static Vendedor cadastrarVendedor(String nome, String email, String cpf){
+    public static Vendedor cadastrarVendedor(String nome, String email, String cpf) {
         Vendedor vendedor = new Vendedor(nome, email, cpf);
         vendedores.add(vendedor);
         return vendedor;
+    }
+
+    //listar todos vendedores - lista de vendedores - fazer exibição percorrendo a lista e printando a referencia
+    //método temporário (?) para testes
+    public static void exibirVendedores() {
+        System.out.println("\nLista de um total de " +vendedores.size() +  " vendedores: ");
+        for (Vendedor referencia : vendedores) {
+            System.out.println(referencia);
+        }
     }
     //verificar se o vendedor está cadastrado - percorrer a lista de vendedores comparando cpf ou email;
 }
